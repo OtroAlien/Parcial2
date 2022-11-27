f5c4b159108d54e7cd4dd1ede7026ca9a10fbd52
ciro <ciroartista@gmail.com>
2022-11-27 02:46:04 -0300

    tendencias

--- index.php
+++ index.php
@@ -143,17 +143,8 @@ include "templates/conexion.php";
                     </div>
                     <h2>Tendencias</h2>
                     <div class="row">
-                        <div class="col-12 col-xs-12 col-md-6 col-lg-6">
-                            <div class="card" style="width: 14rem;">
-                                <img src="img/post_placeholder.jpg" class="card-img-top" alt="tendencias">
-                                <div class="card-body">
-                                    <p class="card-text">Some quick example text to build on the card title and make up
-                                        the bulk of the card's content.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-12 col-xs-12 col-md-6 col-lg-6">
-                            <div class="card" style="width: 14rem;">
+                        <div class="">
+                            <div class="card" >
                                 <img src="img/post_placeholder.jpg" class="card-img-top" alt="tendencias">
                                 <div class="card-body">
                                     <p class="card-text">Some quick example text to build on the card title and make up
